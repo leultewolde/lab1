cb9cacdad015d21c17e2f5d40a630685bfc3fe0a
Leul Tewolde <leulwoldegabriel@gmail.com>
2024-05-27 14:10:15 -0500

    Project setup

---
+++ src/edu/miu/cs/cs489appsd/lab1a/productmgmtapp/ProductMgmtApp.java
@@ -0,0 +1,7 @@
+package edu.miu.cs.cs489appsd.lab1a.productmgmtapp;
+
+public class ProductMgmtApp {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
